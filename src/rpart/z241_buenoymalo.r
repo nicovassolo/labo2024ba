dcdea707335ee455d9649762a76f221ec057656d
nicovassolo <nvassolo@mail.austral.edu.ar>
2024-08-24 15:03:29 +0000

    auto commit 4       20240824 150317

--- src/rpart/z241_buenoymalo.r
+++ src/rpart/z241_buenoymalo.r
@@ -10,12 +10,12 @@ require("ggplot2")
 
 PARAM <- list()
 # reemplazar por su primer semilla
-PARAM$semilla_primigenia <- 102191
+PARAM$semilla_primigenia <- 342821
 PARAM$qsemillas <- 20
 
 # elegir SU dataset comentando/ descomentando
-PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
-# PARAM$dataset_nom <- "~/datasets/conceptual_dataset_pequeno.csv"
+# PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
+PARAM$dataset_nom <- "~/datasets/conceptual_dataset_pequeno.csv"
 
 PARAM$training_pct <- 70L  # entre  1L y 99L 
 
